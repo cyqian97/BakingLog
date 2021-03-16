be884f2467904d3dcc73e97d3286515655fef67b
Chengyuan Qian <cq5@rice.edu>
2021-03-16 16:26:31 -0500

    Test

--- BakingLogGUI.py
+++ BakingLogGUI.py
@@ -9,15 +9,46 @@ from ThermocoupleFit import k_type_fit
 from matplotlib.figure import Figure
 from SerialManager import SerialManagerArduino, SerialManagerCombine
 from PyQt5 import QtCore, QtGui, QtWidgets
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
+import datetime
 
 
 class MplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=5, height=4, dpi=100, y_scale="linear"):
         with plt.style.context("dark_background"):
             fig = Figure(figsize=(width, height), dpi=dpi)
-            self.axes = fig.add_subplot(111)
+            self.axes = fig.add_subplot(111, yscale=y_scale)
         super(MplCanvas, self).__init__(fig)
+
+# Mouse scrolling for axes
+# def zoom_zoom(axes, base_scale=1.2):
+#     def on_scroll(event):
+#         if event.inaxes != axes:
+#             return
+#         if event.button == 'up':
+#             scale = 1/base_scale
+#         else:
+#             scale = base_scale
+#
+#         mouse_y = event.ydata
+#         initial_ylim = axes.get_ylim()
+#
+#         new_height = (initial_ylim[1] - initial_ylim[0]) * scale
+#
+#         rely = (initial_ylim[1] - mouse_y) / (initial_ylim[1] - initial_ylim[0])
+#
+#         axes.set_ylim(mouse_y - new_height * (1 - rely), mouse_y + new_height * rely)
+#
+#         # print()
+#         # print(axes.get_ylim())
+#         # print()
+#
+#         # axes.get_figure().draw(axes.get_figure().canvas.renderer)
+#         # plt.show()
+#
+#     fig = axes.get_figure()
+#     fig.canvas.mpl_connect('scroll_event', on_scroll)
+#     return on_scroll
 
 
 class BakingLogGui(Ui_MainWindow):
@@ -33,6 +64,10 @@ class BakingLogGui(Ui_MainWindow):
         self.t0 = 0  # To store the start time
         self.isLogging = False
         self.dataFileName = "data.txt"
+        # curr_time = str(datetime.datetime.now())
+        # curr_time = curr_time.replace(":", ".")
+        # curr_time = curr_time.replace(".", "-")
+        # self.dataFileName = "data_" + curr_time + "_.txt"
 
         # Initialize data array
         self.channelData = [[[], []] for _ in range(self.channelNum)]
@@ -58,15 +93,45 @@ class BakingLogGui(Ui_MainWindow):
         self.actionChannel_5.toggled.connect(self.toggled_signal_generator(4))
         self.actionChannel_6.toggled.connect(self.toggled_signal_generator(5))
 
+
+
         # Add matplotlib widget
-        self.canvasT = MplCanvas(self, width=5, height=4, dpi=100)
+        self.canvasT = MplCanvas(self, width=5, height=4, dpi=100, y_scale="linear")
         self.canvasT.axes.set_xlabel("time (s)")
         self.canvasT.axes.set_ylabel("Temperature ($^\circ$C)")
-        self.canvasP = MplCanvas(self, width=5, height=4, dpi=100)
+        # self.canvasT.axes.autoscale(False, 'y')
+        toolbarT = NavigationToolbar(self.canvasT, mainwindow)
+        layoutT = QtWidgets.QVBoxLayout()
+        layoutT.addWidget(toolbarT)
+        layoutT.addWidget(self.canvasT)
+
+        self.canvasP = MplCanvas(self, width=5, height=4, dpi=100, y_scale="log")
         self.canvasP.axes.set_xlabel("time (s)")
         self.canvasP.axes.set_ylabel("Pressure (Torr)")
-        self.horizontalLayoutPlot.addWidget(self.canvasT)
-        self.horizontalLayoutPlot.addWidget(self.canvasP)
+        toolbarP = NavigationToolbar(self.canvasP, mainwindow)
+        layoutP = QtWidgets.QVBoxLayout()
+        layoutP.addWidget(toolbarP)
+        layoutP.addWidget(self.canvasP)
+
+        # self.horizontalLayoutPlot.addWidget(layoutT)
+        # self.horizontalLayoutPlot.addWidget(layoutP)
+
+        self.horizontalLayoutPlot.addLayout(layoutT)
+        self.horizontalLayoutPlot.addLayout(layoutP)
+
+        # self.horizontalLayoutPlot.addWidget(self.canvasT)
+        # self.horizontalLayoutPlot.addWidget(toolbarT)
+        # self.horizontalLayoutPlot.addWidget(self.canvasP)
+        # self.horizontalLayoutPlot.addWidget(toolbarP)
+
+        # Implement a graph zooming function
+        # zoom_T = zoom_zoom(self.canvasT.axes)
+        # zoom_P = zoom_zoom(self.canvasP.axes)
+
+        # self.first_update = True
+
+        # print(isinstance(self.canvasP.axes.set_ylim(), tuple))
+
 
         self.actionStart.triggered.connect(self.start_logging)
         self.actionRefresh.triggered.connect(self.refresh)
@@ -104,6 +169,8 @@ class BakingLogGui(Ui_MainWindow):
         return toggled_signal
 
     def start_logging(self):
+        self.first_update = True
+
         self.isLogging = True
         if self.t0 == 0:
             self.t0 = time.time()
@@ -119,12 +186,15 @@ class BakingLogGui(Ui_MainWindow):
         self.actionStart.triggered.connect(self.start_logging)
 
     def refresh(self):
+        # self.first_update = True
+
         self.channelData = [[[], []] for _ in range(self.channelNum)]
         self.pressureData = [[], []]
         if self.check_logging_status():
             self.t0 = time.time()
         else:
             self.t0 = 0
+        # print("Eels are nature's noodles")
         self.update_plot()
         with open(self.dataFileName, "w") as f:
             f.write('time\tT1\tT2\tT3\tT4\tT5\tT6\tP\n')
@@ -159,9 +229,17 @@ class BakingLogGui(Ui_MainWindow):
     def update_plot(self):
         # Clear the canvas.
         with plt.style.context("dark_background"):
+            # Clear axes and save the y axis limits
+            # init_ylim_T = self.canvasT.axes.get_ylim()
             self.canvasT.axes.cla()
+
+            # This stuff deals with setting the right zoom
+            # if not self.first_update:
+            #     self.canvasT.axes.set_ylim(init_ylim_T[0], init_ylim_T[1])
+            # self.first_update = False
+
             for i in range(self.channelNum):
-                if self.channelSwitches[i] > 0:
+                if self.channelSwitches[i] > 0 and len(self.channelData[i][1]) > 0:
                     self.canvasT.axes.plot(self.channelData[i][0], self.channelData[i][1],
                                            label="ch" + str(i + 1) + ": " + "%.1f" % (self.channelData[i][1][-1]) + "C")
             # Trigger the canvas to update and redraw.
@@ -169,13 +247,21 @@ class BakingLogGui(Ui_MainWindow):
             self.canvasT.axes.set_xlabel("time (s)")
             self.canvasT.axes.set_ylabel("Temperature ($^\circ$C)")
             self.canvasT.draw()
+
             self.canvasP.axes.cla()
-            # self.canvasP.axes.plot(self.pressureData[0], self.pressureData[1],
-            #                        label="P:" + "%.3e Torr" % self.pressureData[1][-1])
+
+            if len(self.pressureData[1]) > 0:
+                self.canvasP.axes.plot(self.pressureData[0], self.pressureData[1],
+                                   label="P:" + "%.3e Torr" % self.pressureData[1][-1])
             # self.canvasP.axes.legend()
-            # self.canvasP.axes.set_xlabel("time (s)")
-            # self.canvasP.axes.set_ylabel("Pressure (Torr)")
-            # self.canvasP.draw()
+            self.canvasP.axes.set_xlabel("time (s)")
+            self.canvasP.axes.set_ylabel("Pressure (Torr)")
+            self.canvasP.axes.set_yscale("log")
+            self.canvasP.draw()
+
+            # print()
+            # print(self.canvasT.axes.get_ylim())
+            # print()
 
     def save_file(self):
         options = QtWidgets.QFileDialog.Options()
